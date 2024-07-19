926a012a5fc96ba9bbee7659b22bf7d1c604d9b2
Hiba-Mohamed <hibamoha987@gmail.com>
2024-07-19 15:56:33 -0230

    initial structure

---
+++ dog-image-gallery/src/App.js
@@ -0,0 +1,18 @@
+import "./index.css";
+import Header from "./components/Header";
+import BreedSelector from "./components/BreedSelector";
+import ImageGallery from "./components/ImageGallery";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <BreedSelector />
+      <ImageGallery />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
