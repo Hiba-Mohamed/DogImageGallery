a6b51d677865f84de6ce5b39c408417a0ac7fbe4
Hiba-Mohamed <hibamoha987@gmail.com>
2024-07-20 14:29:59 -0230

    conditional length alert menssage

--- dog-image-gallery/src/components/ImageGallery.jsx
+++ dog-image-gallery/src/components/ImageGallery.jsx
@@ -3,13 +3,20 @@ import React from 'react'
 const ImageGallery = ({ imagesArray, numberOfImages }) => {
   console.log(imagesArray, numberOfImages);
   const imagesWithSpecifiedNumber = imagesArray.slice(0, numberOfImages);
+  const availableImagesNumber = imagesArray.length
+  console.log(availableImagesNumber)
   console.log(imagesWithSpecifiedNumber)
   return (
     <div>
-      <h2 className='galleryTitle'>Image Gallery</h2>
-      <div className='imagesDiv'>
+      <h2 className="galleryTitle">Image Gallery</h2>
+      {numberOfImages > imagesArray.length && (
+        <p className='lengthAlertP'>
+          Only {imagesArray.length} Picture available for the breed selected
+        </p>
+      )}
+      <div className="imagesDiv">
         {imagesWithSpecifiedNumber.map((link) => (
-            <img src={link} alt="picture"  />
+          <img src={link} alt="picture" />
         ))}{" "}
       </div>
     </div>
